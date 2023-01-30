808caa558b2888e44aa3a566c757fde7370f94fb
Kevin Gonzalez <kevjetsky@gmail.com>
2023-01-30 01:33:24 -0700

    [add] database name in database setting file

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,11 +1,12 @@
 import { connect } from 'mongoose';
 
 // Replace <<NameDatabase>> for your local database name
-const URI = 'mongodb://0.0.0.0/<<NameDatabase>>';
+const URI = 'mongodb://0.0.0.0/full-auth';
 
 connect(URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 })
     .then(db => console.log(">>> Database is connected"))
     .catch(err => console.error(err));
