45b29c4f94b1eee403566ac15c5426599b6520f3
Kevin Gonzalez <kevjetsky@gmail.com>
2023-01-30 02:25:56 -0700

    [add] userRoutes to main routes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import userRoutes from './api/User/routes';
 
 // inicialization
 const app = express();
 
 // routes
-
+app.use('/user', userRoutes);
 
 export default app;
